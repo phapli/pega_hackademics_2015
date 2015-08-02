f4d62c4a2460cb58f229bbc81b176dedca01cf97
dinhphi2205 <dinhphi2205@gmail.com>
2015-08-02 09:33:45 +0700

    + add ActivitySetting

--- android/SchoolTaskList/app/src/main/java/schooltasklist/pega/com/config/Configs.java
+++ android/SchoolTaskList/app/src/main/java/schooltasklist/pega/com/config/Configs.java
@@ -9,4 +9,8 @@ public class Configs {
     //AIzaSyCU7HvSJ5nO7ZqPlCLd-Nm_LMOu-Benxhk
     //Sender ID
     //956133373662
+
+    public static final String MY_PREFERENCES = "PegaPrefs" ;
+    public static final String PUSH = "pushnotification";
+    public static final String TIME_REMINDER = "timeremind";
 }
